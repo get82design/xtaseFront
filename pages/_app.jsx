e1a8a2e689b8ff177f1f42aec1195ae82d1eded7
Getup <get82design@gmail.com>
2023-05-01 23:11:56 +0200

    global populate

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -68,15 +68,15 @@ MyApp.getInitialProps = async (ctx) => {
   // Calls page's `getInitialProps` and fills `appProps.pageProps`
   // const appProps = await App.getInitialProps(ctx);
   // Fetch global site settings from Strapi
-  const globalRes = await fetchAPIWithoutLocale("/global", {
-    populate:
-    {
-      favicon: "*",
-      logo: "*",
-      defaultSeo: {
-        populate: "*",
-      }
-    },
+  const globalRes = await fetchAPI("/global", {
+    populate: "*"
+    // {
+    //   favicon: "*",
+    //   logo: "*",
+    //   defaultSeo: {
+    //     populate: "*",
+    //   }
+    // },
   });
   // Pass the data to our page via props
   return { pageProps: { global: globalRes.data } };
